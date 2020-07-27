c9052078e18d3fdac92b22095fa4c22997df8e10
Raaj52 <mail2raaj52@gmail.com>
2020-07-27 13:16:55 +0530

    raphael: prebuilts: Add GCam GO
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_PACKAGES += \
 #    lineage.livedisplay@2.0-service-sdm
 
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    GCamGo
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
