8648824a7c0a1c87dc035342675db1c8cdce86a7
ejbtrd <ejbtrd@gmail.com>
2020-10-23 22:33:11 +0000

    raphael: Initial Toxyc changes
    Signed-off-by: ejbtrd <ejbtrd@gmail.com>

--- device.mk
+++ device.mk
@@ -111,14 +111,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl
 
-# Livedisplay
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl.raphael
-
-# Livedisplay
-PRODUCT_PACKAGES += \
-    lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
--- toxyc_raphael.mk
+++ toxyc_raphael.mk
@@ -6,17 +6,17 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common.mk)
+# Inherit some common Toxyc stuff.
+$(call inherit-product, vendor/toxyc/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := awaken_raphael
-
-BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
+PRODUCT_NAME := toxyc_raphael
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-AWAKEN_BUILD_TYPE := OFFICIAL
+# Hazard zone
+TARGET_BOOT_ANIMATION_RES := 1080
+TOXYC_OFFICIAL := true
